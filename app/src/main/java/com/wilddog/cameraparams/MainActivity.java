6c323274c5b8570f85ecf9f380313db77b50772f
董笑飞 <dongxiaofei@wilddog.com>
2017-04-21 15:43:43 +0800

    delete retry

--- app/src/main/java/com/wilddog/cameraparams/MainActivity.java
+++ app/src/main/java/com/wilddog/cameraparams/MainActivity.java
@@ -22,10 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
         int cameraCount = Camera.getNumberOfCameras();
         Log.e(TAG, Build.MODEL+"cameraCount:"+cameraCount);
-      /*  for(int i =0;i<cameraCount;i++){*/
-      int i;
+        for(int i =0;i<cameraCount;i++){
             try{
-                i=1;
             camera = Camera.open(i);
             camera.lock();
                 Camera.CameraInfo info = new Camera.CameraInfo();
@@ -56,12 +54,10 @@ public class MainActivity extends AppCompatActivity {
             camera=null;}
             catch (Exception e){
                 // 部分手机会出错
-                Log.e(TAG,"camera"+1+"can't get camera");
+                Log.e(TAG,"camera"+i+"can't get camera");
                 e.printStackTrace();
+
             }
-        /*}*/
+        }
     }
-
-
-
 }
